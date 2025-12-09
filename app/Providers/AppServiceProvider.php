bafe8eba0987fe82e0eeba3330be3acc9be9be15
3041TAN-16 <3041TAN-16@tanuzem.boronkay.local>
2025-12-09 14:42:55 +0100

    client

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Http::macro('api', function () {
+            return Http::withHeaders([
+                'Accept' => 'application/json',
+            ])->baseUrl((string) config('services.api.base_uri'));
+        });
+        
     }
 }
--- config/services.php
+++ config/services.php
@@ -34,5 +34,10 @@ return [
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
     ],
+    
+    'api' => [
+        'base_uri' => env('API_URL', 'https://api.example.com/api'),
+    ],
+
 
 ];
